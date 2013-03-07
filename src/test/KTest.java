035fde06239b3cd1ff6e09272c4825b16cb57523
OlavOlseng <olseng.o@gmail.com>
2013-03-07 13:12:39 +0100

    Cleanup

--- src/test/KTest.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-public class KTest
-	{
-	public static void main(String args[])
-		{
-		System.out.println("Hei! Dette er en test fra Kenneth.");
-		}
-	}
--- src/test/fTest.java
+++
@@ -1,8 +0,0 @@
-package test;
-
-public class fTest {
-
-	public static void main(String[] args) {
-		System.out.println("Test");
-	}
-}
