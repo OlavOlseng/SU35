bab560eba4f961a6c45c8a4c7419b594f02dde25
sindrma <sindrma@gmail.com>
2013-03-14 15:31:42 +0100

    Changes in util folder, deleted XMLSerializer and added XMLAssembler for translating objects. SocketTest minor changes

--- client/src/util/XmlTest.java
+++ client/src/util/XmlTest.java
@@ -12,26 +12,13 @@ public class XmlTest {
 	public static void main(String[] args) {
 		Employee meg = new Employee("sindrma@gmail.com", "Sindre", "Magnussen", 
 				"69167663", "41517055");
-		try {
-			String xml = XMLSerializer.personToXml(meg);
-			System.out.println(xml);
-			Employee emp = XMLSerializer.toPerson(xml);
-			System.out.println(emp);
-		} catch (ParserConfigurationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ParsingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-
+		XMLFactory.createNewObject("employee");
+		XMLFactory.addChild("email", meg.getEmail());
+		XMLFactory.addChild("firstname", meg.getFirstName());
+		XMLFactory.addChild("lastname", meg.getLastName());
+		XMLFactory.addChild("homephone", meg.getHomePhone());
+		XMLFactory.addChild("mobilephone", meg.getMobilePhone());
+		System.out.println(XMLFactory.getXML());
 	}
 
 }
