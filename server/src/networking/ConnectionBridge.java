df79bd747301d304a2d1b7cfb958ed7534bca0e7
augustak <augustak@stud.ntnu.no>
2013-03-14 13:18:42 +0100

    Added Factory.java, made some changes to ConnectionBridge

--- server/src/networking/ConnectionBridge.java
+++ server/src/networking/ConnectionBridge.java
@@ -1,6 +1,9 @@
 package networking;
 
+import dbinterface.*;
+import models.*;
 import java.io.IOException;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import dbinterface.DBConnection;
@@ -9,11 +12,13 @@ public class ConnectionBridge implements MessageListener {
 
 	private Connection inetConnection;
 	private DBConnection dbConnection;
+	private Factory factory;
 	private ReceiveWorker rw;
 
 	public ConnectionBridge(Connection c, DBConnection dbc) {
 		this.inetConnection = c;
 		this.dbConnection = dbc;
+		this.factory = new Factory();
 		this.rw = new ReceiveWorker(c);
 		rw.addListener(this);
 		rw.start();
@@ -39,6 +44,12 @@ public class ConnectionBridge implements MessageListener {
 		// TODO Auto-generated method stub
 		System.out.println(msg);
 		try {
+			send(makeEmployeeQuery(msg));
+		}
+		catch(IOException ioe) {
+			System.out.println(ioe.getMessage());
+		}
+		try {
 			ConnectionPool.getInstance().broadcast(String.format("Message received from %s: %s", inetConnection.getRemoteAddress().getHostAddress(), msg));
 			if(msg == null) 
 				close();
@@ -49,5 +60,20 @@ public class ConnectionBridge implements MessageListener {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	private String makeEmployeeQuery(String email) {
+		Employee employee = null;
+		try {
+			ResultSet rs = dbConnection.makeSingleQuery("SELECT * FROM Employee WHERE email=" + email);
+			employee = factory.getEmployee(rs);
+		}
+		catch(SQLException sqle) {
+			System.out.println(sqle.getMessage());
+		}
+		catch(ClassNotFoundException cnfe) {
+			System.out.println(cnfe.getMessage());
+		}
+		return employee.toString();
 	}
 }
