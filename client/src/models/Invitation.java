ff54330557ff01e8a1e8bb9494a2e8583e8b0eb0
OlavOlseng <olseng.o@gmail.com>
2013-03-15 16:52:05 +0100

    Adapted model to work properly

--- client/src/models/Invitation.java
+++ client/src/models/Invitation.java
@@ -1,33 +1,54 @@
 package models;
 
 public class Invitation {
-	public enum Answer { ACCEPTED, DECLINED, PENDING};
+	public enum Answer { 
+		ACCEPTED(1), 
+		DECLINED(0), 
+		PENDING(-1);
+
+		int b;
+
+		Answer(int b) {
+			this.b = b;
+		}
+	}
+
 	private Answer answer;
 	private final String employeeEmail;
 	private final int appointmentID;
+	String message;
 	
 	public Invitation(String employeeEmail, int appointmentID) {
 		this.employeeEmail = employeeEmail;
 		this.appointmentID = appointmentID;
 		answer = Answer.PENDING;
+
 	}
-	
+
 	public String getEmployeeEmail() {
 		return employeeEmail;
 	}
-	
+
 	public int getAppointmentID() {
 		return appointmentID;
 	}
-	
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
 	public void setAnswer(Answer answer){
 		this.answer = answer;
 	}
-	
+
 	public Answer getAnswer() {
 		return answer;
 	}
-	
+
 	public String toString() {
 		if(answer == Answer.ACCEPTED) {
 			return "Invitation accepted";
