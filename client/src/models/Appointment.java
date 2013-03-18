ede0ab55cb7756740c175d4a3b5c7f2adfb1796f
kennethpl <acaykon@online.no>
2013-03-18 17:45:00 +0100

    Noen endringer

--- client/src/models/Appointment.java
+++ client/src/models/Appointment.java
@@ -11,7 +11,7 @@ public class Appointment {
 	private String location;
 	private String meetingRoom;
 	private String meetingLeader;
-	
+
 	public Appointment(int appointmentID) {
 		this.appointmentID = appointmentID;
 		description = "";
@@ -20,15 +20,15 @@ public class Appointment {
 		endTime = Calendar.getInstance();
 		date = Calendar.getInstance();
 	}
-	
+
 	public void setAppointmentID(int appointmentID){
 		this.appointmentID = appointmentID;
 	}
-	
+
 	public int getAppointmentID() {
 		return appointmentID;
 	}
-	
+
 	public String getMeetingRoom() {
 		return meetingRoom;
 	}
@@ -36,7 +36,7 @@ public class Appointment {
 	public void setMeetingRoom(String meetingRoom) {
 		this.meetingRoom = meetingRoom;
 	}
-
+	
 	public String getMeetingLeader() {
 		return meetingLeader;
 	}
@@ -44,40 +44,40 @@ public class Appointment {
 	public void setMeetingLeader(String meetingLeader) {
 		this.meetingLeader = meetingLeader;
 	}
-
+	
 	private void setDate(int year, int month, int day) {
 		date.set(year, month-1, day, 0, 0, 0);
 		startTime.set(year, month-1, day);
 		endTime.set(year, month-1, day);
 	}
-	
+
 	public void setDate(String dateString) {
 		String[] temp = dateString.split("-");
 		setDate(Integer.parseInt(temp[0]), Integer.parseInt(temp[1]), Integer.parseInt(temp[2]));
 	}
-	
+
 	private void setStartTime(int hour, int minute) {
 		startTime.setTime(date.getTime());
 		startTime.set(Calendar.HOUR_OF_DAY, hour);
 		startTime.set(Calendar.MINUTE, minute);
 	}
-	
+
 	public void setStartTime(String time) {
 		String[] temp = time.split(":");
 		setStartTime(Integer.parseInt(temp[0]), Integer.parseInt(temp[1]));
 	}
-	
+
 	private void setEndTime(int hour, int minute) {
 		endTime.setTime(date.getTime());
 		endTime.set(Calendar.HOUR_OF_DAY, hour);
 		endTime.set(Calendar.MINUTE, minute);
 	}
-	
+
 	public void setEndTime(String time) {
 		String[] temp = time.split(":");
 		setEndTime(Integer.parseInt(temp[0]), Integer.parseInt(temp[1]));
 	}
-	
+
 	public String getFormattedDate() {
 		String s = "";
 		s += (date.getTime().getYear()+1900) + "-";
@@ -93,7 +93,7 @@ public class Appointment {
 		s += (day);
 		return s;
 	}
-	
+
 	public String getFormattedStartTime() {
 		String s = "";
 		int hours = startTime.getTime().getHours();
@@ -108,7 +108,7 @@ public class Appointment {
 		s += minutes;
 		return s;
 	}
-	
+
 	public String getFormattedEndTime() {
 		String s = "";
 		int hours = endTime.getTime().getHours();
@@ -123,42 +123,42 @@ public class Appointment {
 		s += minutes;
 		return s;
 	}
-	
+
 	public Calendar getDate() {
 		return date;
 	}
-	
+
 	public Calendar getStartTime() {
 		return startTime;
 	}
-	
+
 	public Calendar getEndTime() {
 		return endTime;
 	}
-	
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	
+
 	public String getDescription() {
 		return description;
 	}
-	
+
 	public void setLocation(String location) {
 		this.location = location;
 	}
-	
+
 	public String getLocation() {
 		return location;
 	}
-	
+
 	// May cause null-pointer hell, too lazy to fix. <--- TRUE DAT
 	public String toString() {
-		String s  = "Appointment ID:\t\t\t" + appointmentID + "\nAppointment Description:\t" + description +
+		String s = "Appointment ID:\t\t\t" + appointmentID + "\nAppointment Description:\t" + description +
 				"\nStart time:\t\t\t" + startTime.getTime().toString() + "\nEnd time:\t\t\t" + endTime.getTime().toString() + 
 				"\nAppointment Location:\t\t" + location;
 		if (meetingRoom != null && meetingRoom != "")
-				s +=  "\nMeeting room:\t\t\t" + meetingRoom;
+				s += "\nMeeting room:\t\t\t" + meetingRoom;
 		if(meetingLeader != null && meetingLeader != "")
 				s += "\nMeeting Leader:\t\t\t" + meetingLeader;
 		return s;
