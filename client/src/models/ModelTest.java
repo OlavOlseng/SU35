b65a89df41aa47d8b0a02e2ab88206d4304fbbea
augustak <augustak@stud.ntnu.no>
2013-03-19 15:21:54 +0100

    added more testing functionality in ModelTest & added more queries to Client

--- client/src/models/ModelTest.java
+++ client/src/models/ModelTest.java
@@ -6,73 +6,11 @@ import networking.*;
 
 public class ModelTest {
 	public static void main(String args[]) {
-		Appointment app1 = new Appointment(1);
-		app1.setDate("2013-03-18");
-		app1.setDescription("Møte 1");
-		app1.setLocation("NTNU");
-		app1.setStartTime("12:15");
-		app1.setEndTime("13:15");
-		app1.setMeetingLeader("olav@epost.no");
-		app1.setMeetingRoom("Drivhuset");
-		//ApplicationModel.getInstance().addAppointment(1, app1);
-		
-		Appointment app2 = new Appointment(2);
-		app2.setDate("2013-03-18");
-		app2.setDescription("Møte 2");
-		app2.setLocation("NTNU");
-		app2.setStartTime("12:55");
-		app2.setEndTime("13:55");
-		app2.setMeetingLeader("august@epost.no");
-		app2.setMeetingRoom("R-bygget R-7");
-		//ApplicationModel.getInstance().addAppointment(2, app2);
-		
-		Appointment app3 = new Appointment(3);
-		app3.setDate("2013-03-18");
-		app3.setDescription("Møte 3");
-		app3.setLocation("NTNU");
-		app3.setStartTime("12:00");
-		app3.setEndTime("13:00");
-		app3.setMeetingLeader("sindre@epost.no");
-		app3.setMeetingRoom("Stripa S-1");
-		
-		Room room1 = new Room("Drivhuset", 30);
-		Room room2 = new Room("R-bygget R-7", 500);
-		Room room3 = new Room("Stripa S-1", 200);
-		
-		Appointment app4 = new Appointment(4);
-		app4.setDate("2013-03-18");
-		app4.setDescription("Møte 4");
-		app4.setLocation("NTNU");
-		app4.setStartTime("13:05");
-		app4.setEndTime("14:05");
-		app4.setMeetingLeader("fredrik@epost.no");
-		
-		RoomManager roomManager = new RoomManager(6, app4.getDate(), app4.getStartTime(), app4.getEndTime());
-		roomManager.addAppointment(app1);
-		roomManager.addAppointment(app2);
-		roomManager.addAppointment(app3);
-		roomManager.addRoom(room1.getName(), room1.getRoomSize());
-		roomManager.addRoom(room2.getName(), room2.getRoomSize());
-		roomManager.addRoom(room3.getName(), room3.getRoomSize());
-		
-		String room = roomManager.pickSuitableRoom();
-		app4.setMeetingRoom(room);
-		
-		System.out.println(app4.toString());
-		System.out.println(app4.getFormattedDate());
-		System.out.println(app4.getFormattedStartTime());
-		System.out.println(app4.getFormattedEndTime());
-		
-		Alarm alarm = new Alarm(2, "fredrik@epost.no");
-		alarm.setDescription("Kebab fo shizzles!");
-		alarm.setTime("15");
-		
 		Client client = new Client();
-		System.out.println("Connecting to server...");
 		client.connect();
-		System.out.println("Connected to " + client.getHostAddress());
 		Scanner scanner = new Scanner(System.in);
 		String command = null;
+		ApplicationModel app = ApplicationModel.getInstance();
 		boolean exit = false;
 		do {
 			command = scanner.nextLine();
@@ -81,60 +19,187 @@ public class ModelTest {
 					exit = true;
 				} break;
 				case "GET_EMPLOYEE": {
+					System.out.print("Employee email: ");
 					String email = scanner.nextLine();
 					client.sendEmployeeQuery(email);
 				} break;
 				case "GET_APPOINTMENT": {
+					System.out.print("Appointment ID: ");
 					String ID = scanner.nextLine();
 					client.sendAppointmentQuery(ID);
 				} break;
 				case "GET_INVITATION": {
+					System.out.print("Employee email: ");
 					String email = scanner.nextLine();
 					if(email.equals("")) email = null;
+					System.out.print("Appointment ID: ");
 					String ID = scanner.nextLine();
 					if(ID.equals("")) ID = null;
 					client.sendInvitationQuery(email, ID);
 				} break;
 				case "GET_ROOM": {
+					System.out.print("Room name: ");
 					String name = scanner.nextLine();
 					client.sendRoomQuery(name);
 				} break;
 				case "GET_ALARM": {
+					System.out.print("Employee email: ");
 					String email = scanner.nextLine();
+					System.out.print("Appointment ID: ");
 					String ID = scanner.nextLine();
 					client.sendAlarmQuery(email, ID);
 				} break;
 				case "GET_GROUP": {
+					System.out.print("Group email: ");
 					String email = scanner.nextLine();
 					client.sendGroupQuery(email);
 				} break;
+				case "DELETE_EMPLOYEE": {
+					System.out.print("Employee email: ");
+					String email = scanner.nextLine();
+					client.sendEmployeeDeletion(email);
+				} break;
 				case "DELETE_APPOINTMENT": {
+					System.out.print("Appointment ID: ");
 					String ID = scanner.nextLine();
 					client.sendAppointmentDeletion(ID);
 				} break;
 				case "DELETE_INVITATION": {
+					System.out.print("Employee email: ");
 					String email = scanner.nextLine();
+					System.out.print("Appointment ID: ");
 					String ID = scanner.nextLine();
 					client.sendInvitationDeletion(email, ID);
 				} break;
+				case "DELETE_ROOM": {
+					System.out.print("Room name: ");
+					String name = scanner.nextLine();
+					client.sendRoomDeletion(name);
+				} break;
 				case "DELETE_ALARM": {
+					System.out.print("Employee email: ");
 					String email = scanner.nextLine();
+					System.out.print("Appointment ID: ");
 					String ID = scanner.nextLine();
 					client.sendAlarmDeletion(email, ID);
 				} break;
 				case "CREATE_ALARM": {
+					System.out.print("Employee email: ");
+					String email = scanner.nextLine();
+					System.out.print("Appointment ID: ");
+					int ID = Integer.parseInt(scanner.nextLine());
+					System.out.print("Alarm time: ");
+					String time = scanner.nextLine();
+					System.out.print("Alarm description: ");
+					String description = scanner.nextLine();
+					Alarm alarm = new Alarm(ID, email);
+					alarm.setTime(time);
+					alarm.setDescription(description);
 					client.sendAlarmCreation(alarm);
 				} break;
+				case "UPDATE_EMPLOYEE": {
+					System.out.print("Employee email: ");
+					String email = scanner.nextLine();
+					System.out.print("Employee first name: ");
+					String first = scanner.nextLine();
+					System.out.print("Employee last name: ");
+					String last = scanner.nextLine();
+					System.out.print("Employee homephone: ");
+					String home = scanner.nextLine();
+					System.out.print("Employee mobilephone: ");
+					String mobile = scanner.nextLine();
+					Employee employee = new Employee(email, first, last, home, mobile);
+					client.sendEmployeeUpdate(employee);
+				} break;
+				case "UPDATE_APPOINTMENT": {
+					System.out.print("Appointment ID: ");
+					int ID = Integer.parseInt(scanner.nextLine());
+					System.out.print("Appointment title: ");
+					String title = scanner.nextLine();
+					System.out.print("Appointment date: ");
+					String date = scanner.nextLine();
+					System.out.print("Appointment start time: ");
+					String startTime = scanner.nextLine();
+					System.out.print("Appointment end time: ");
+					String endTime = scanner.nextLine();
+					System.out.print("Appointment description: ");
+					String description = scanner.nextLine();
+					System.out.print("Appointment location: ");
+					String location = scanner.nextLine();
+					System.out.print("Appointment meeting leader: ");
+					String leader = scanner.nextLine();
+					System.out.print("Appointment meeting room: ");
+					String room = scanner.nextLine();
+					Appointment appointment = new Appointment(ID);
+					appointment.setTitle(title);
+					appointment.setDate(date);
+					appointment.setStartTime(startTime);
+					appointment.setEndTime(endTime);
+					appointment.setDescription(description);
+					appointment.setLocation(location);
+					appointment.setMeetingLeader(leader);
+					appointment.setMeetingRoom(room);
+					client.sendAppointmentUpdate(appointment);
+				} break;
+				case "UPDATE_INVITATION": {
+					System.out.print("Employee email: ");
+					String email = scanner.nextLine();
+					System.out.print("Appointment ID: ");
+					int ID = Integer.parseInt(scanner.nextLine());
+					System.out.print("Invitation answer: ");
+					String answer = scanner.nextLine();
+					System.out.print("Invitation message: ");
+					String message = scanner.nextLine();
+					Invitation invitation = new Invitation(email, ID);
+					switch(answer) {
+						case "ACCEPTED": {
+							invitation.setAnswer(Invitation.Answer.ACCEPTED);
+						} break;
+						case "DECLINED": {
+							invitation.setAnswer(Invitation.Answer.DECLINED);
+						} break;
+						default: {
+							invitation.setAnswer(Invitation.Answer.PENDING);
+						} break;
+					}
+					invitation.setMessage(message);
+					client.sendInvitationUpdate(invitation);
+				} break;
+				case "UPDATE_ROOM": {
+					System.out.print("Room name: ");
+					String email = scanner.nextLine();
+					System.out.print("Room size: ");
+					int size = Integer.parseInt(scanner.nextLine());
+					Room room = new Room(email, size);
+					client.sendRoomUpdate(room);
+				} break;
+				case "UPDATE_ALARM": {
+					System.out.print("Employee email: ");
+					String email = scanner.nextLine();
+					System.out.print("Appointment ID: ");
+					int ID = Integer.parseInt(scanner.nextLine());
+					System.out.print("Alarm time: ");
+					String time = scanner.nextLine();
+					System.out.print("Alarm description: ");
+					String description = scanner.nextLine();
+					Alarm alarm = new Alarm(ID, email);
+					alarm.setTime(time);
+					alarm.setDescription(description);
+					client.sendAlarmUpdate(alarm);
+				} break;
+				case "SHOW_ALARM": {
+					System.out.print("Employee email: ");
+					String email = scanner.nextLine();
+					System.out.print("Appointment ID: ");
+					int ID = Integer.parseInt(scanner.nextLine());
+					Alarm alarm = app.getAlarm(email, ID);
+					System.out.println(alarm.toString());
+				}
 				default: {
 					System.err.println("UNKNOWN COMMAND");
 				} break;
 			}
 		} while(!exit);
-		/*
-		String msg = "Per#Mathias".split("#")[0];
-		System.out.println(msg);
-		*/
-		System.out.println("Closing connection to server...");
 		client.disconnect();
 	}
 }
--- client/src/networking/Client.java
+++ client/src/networking/Client.java
@@ -12,7 +12,7 @@ public class Client implements MessageListener {
 	private ClientMessageHandler messageHandler;
 	private XMLFactory xmlFactory;
 	private final static int CONNECTPORT = 49153;
-	private final static String HOST = "78.91.12.42";
+	private final static String HOST = "78.91.15.205";
 	
 	public Client() {
 		try {
@@ -29,16 +29,19 @@ public class Client implements MessageListener {
 	
 	public void connect() {
 		try {
+			System.out.println("Connecting to " + getHostAddress());
 			connection.connect(HOST, CONNECTPORT);
 			receiveWorker.start();
 		}
 		catch(IOException ioe) {
 			System.err.println(ioe.getMessage());
 		}
+		System.out.println("Successfully connected to " + getHostAddress());
 	}
 	
 	public void disconnect() {
 		try {
+			System.out.println("Closing connection to " + getHostAddress());
 			if(!connection.isClosed()) {
 				connection.close();
 			}
@@ -46,15 +49,15 @@ public class Client implements MessageListener {
 		catch(IOException ioe) {
 			System.err.println(ioe.getMessage());
 		}
+		System.out.println("Connection to " + getHostAddress() + " closed successfully");
 	}
 	
 	public void messageReceived(String message) {
-		System.out.println(message);
-		//messageHandler.handleMessage(message);
+		messageHandler.handleMessage(message);
 	}
 	
 	public String getHostAddress() {
-		return connection.getRemoteAddress().getHostAddress();
+		return HOST + ":" + CONNECTPORT;
 	}
 	
 	public void send(String msg) {
@@ -120,28 +123,121 @@ public class Client implements MessageListener {
 	/*
 	 * QUERY THE SERVER FOR DELETION
 	 */
+	public void sendEmployeeDeletion(String email) {
+		Employee employee = new Employee(email,null,null,null,null);
+		String payload = xmlFactory.makeEmployeeXML(employee);
+		String msg = factory.makeDeleteMessage(SBPFactory.OPTION_EMPLOYEE, payload);
+		send(msg);
+	}
+	
 	public void sendAppointmentDeletion(String ID) {
-		String msg = factory.makeDeleteMessage(SBPFactory.OPTION_APPOINTMENT, ID);
+		Appointment appointment = new Appointment(Integer.parseInt(ID));
+		String payload = xmlFactory.makeAppointmentXML(appointment);
+		String msg = factory.makeDeleteMessage(SBPFactory.OPTION_APPOINTMENT, payload);
 		send(msg);
 	}
 	
 	public void sendInvitationDeletion(String email, String ID) {
-		String msg = factory.makeDeleteMessage(SBPFactory.OPTION_INVITATION, email+"¤"+ID);
+		Invitation invitation = new Invitation(email, Integer.parseInt(ID));
+		String payload = xmlFactory.makeInvitationXML(invitation);
+		String msg = factory.makeDeleteMessage(SBPFactory.OPTION_INVITATION, payload);
 		send(msg);
 	}
 	
+	public void sendRoomDeletion(String name) {
+		Room room = new Room(name, 0);
+		String payload = xmlFactory.makeRoomXML(room);
+		String msg = factory.makeDeleteMessage(SBPFactory.OPTION_ROOM, payload);
+		send(msg);
+	}
+	
+	/*
+	public void sendGroupDeletion(String email) {
+		Group group = new Group(email);
+		String payload = xmlFactory.makeGroupXML(group);
+		String msg = factory.makeDeleteMessage(SBPFactory.OPTION_GROUP, payload);
+		send(msg);
+	}
+	*/
+	
 	public void sendAlarmDeletion(String email, String ID) {
-		String msg = factory.makeDeleteMessage(SBPFactory.OPTION_ALARM, email+"¤"+ID);
+		Alarm alarm = new Alarm(Integer.parseInt(ID), email);
+		String payload = xmlFactory.makeAlarmXML(alarm);
+		String msg = factory.makeDeleteMessage(SBPFactory.OPTION_ALARM, payload);
 		send(msg);
 	}
 	
 	/*
 	 * QUERY THE SERVER FOR CREATION
 	 */
+	public void sendEmployeeCreation(Employee employee) {
+		String payload = xmlFactory.makeEmployeeXML(employee);
+		String msg = factory.makeCreateMessage(SBPFactory.OPTION_EMPLOYEE, payload);
+		send(msg);
+	}
+	
+	public void sendAppointmentCreation(Appointment appointment) {
+		String payload = xmlFactory.makeAppointmentXML(appointment);
+		String msg = factory.makeCreateMessage(SBPFactory.OPTION_APPOINTMENT, payload);
+		send(msg);
+	}
+	
+	public void sendInvitationCreation(Invitation invitation) {
+		String payload = xmlFactory.makeInvitationXML(invitation);
+		String msg = factory.makeCreateMessage(SBPFactory.OPTION_INVITATION, payload);
+		send(msg);
+	}
+	
+	public void sendRoomCreation(Room room) {
+		String payload = xmlFactory.makeRoomXML(room);
+		String msg = factory.makeCreateMessage(SBPFactory.OPTION_ROOM, payload);
+		send(msg);
+	}
+	
+	/*
+	public void sendGroupCreation(Group group) {
+		String payload = xmlFactory.makeGroupXML(group);
+		String msg = factory.makeCreateMessage(SBPFactory.OPTION_GROUP, payload);
+		send(msg);
+	}
+	*/
+	
 	public void sendAlarmCreation(Alarm alarm) {
 		String payload = xmlFactory.makeAlarmXML(alarm);
-		System.out.println(payload);
-		String msg = factory.createMessage(SBPFactory.MessageType.CREATE, false, "...", SBPFactory.OPTION_ALARM, payload);
+		String msg = factory.makeCreateMessage(SBPFactory.OPTION_ALARM, payload);
+		send(msg);
+	}
+	
+	/*
+	 * QUERY THE SERVER FOR UPDATE
+	 */
+	public void sendEmployeeUpdate(Employee employee) {
+		String payload = xmlFactory.makeEmployeeXML(employee);
+		String msg = factory.makeUpdateMessage(SBPFactory.OPTION_EMPLOYEE, payload);
+		send(msg);
+	}
+	
+	public void sendAppointmentUpdate(Appointment appointment) {
+		String payload = xmlFactory.makeAppointmentXML(appointment);
+		String msg = factory.makeUpdateMessage(SBPFactory.OPTION_APPOINTMENT, payload);
+		send(msg);
+	}
+	
+	public void sendInvitationUpdate(Invitation invitation) {
+		String payload = xmlFactory.makeInvitationXML(invitation);
+		String msg = factory.makeUpdateMessage(SBPFactory.OPTION_INVITATION, payload);
+		send(msg);
+	}
+	
+	public void sendRoomUpdate(Room room) {
+		String payload = xmlFactory.makeRoomXML(room);
+		String msg = factory.makeUpdateMessage(SBPFactory.OPTION_ROOM, payload);
+		send(msg);
+	}
+	
+	public void sendAlarmUpdate(Alarm alarm) {
+		String payload = xmlFactory.makeAlarmXML(alarm);
+		String msg = factory.makeUpdateMessage(SBPFactory.OPTION_ALARM, payload);
 		send(msg);
 	}
 }
